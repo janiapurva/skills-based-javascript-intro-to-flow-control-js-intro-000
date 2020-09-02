775321d9408838e6043ab43c8d7fee3973d423b2
APURVA MAHNEDRABHAI JANI <jani.apurva@yahoo.com>
2020-09-02 02:15:47 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,16 +1,38 @@
-
 function basicTeenager(age) {
-if (age>=13 && =<19)}
-return ("youAreaTeenager"!)
+if (age >= 13 && age <= 19) {
+return 'You are a teenager!'}
 }
-age) { if ()
 
+function teenager(age) {
+if (age <= 13 && age <= 19) {
+return "You are a teenager!"}
+else {
+return "You are not a teenager"}
+}
+
+function ageChecker(age) {
+if (age >= 13 && age <= 19) {
+return "You are a teenager!"}
+else if (age <= 12) {
+return "You are a kid" }
+else (age >= 20) {
+return "You are a grownup"}
 }
 
 function ternaryTeenager(age) {
-
+if (age >= 13 && age <= 19) {
+return "You are a teenager"
+}
+return "You are not a teenager"
 }
 
 function switchAge(age) {
-
+switch(age) {
+case (age >= 13 && age <= 19):
+return ("You are a teenager");
+break;
+default:
+return ('You have an age')
+break;
+}
 }
